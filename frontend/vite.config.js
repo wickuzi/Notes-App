f59db9258d114df5c9cf0bceb048728311ed429c
David Rocha <davidrochadexmg12@gmail.com>
2025-07-25 17:18:51 -0600

    fixing frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  },
+  preview: {
+    port: 5173,
+    strictPort: true
+  }
 })
