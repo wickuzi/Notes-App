14e53c7dccca2fd457cb80b61975f10c8fa7576c
David Rocha <davidrochadexmg12@gmail.com>
2025-07-25 17:00:12 -0600

    fixed

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,16 @@
+from django.http import HttpResponse  # Añade esto al inicio
 from api.views import createUserView
 from api.views import MyTokenObtainPairView
-from rest_framework_simplejwt.views import TokenRefreshView  # ← FALTABA ESTA IMPORTACIÓN
+from rest_framework_simplejwt.views import TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("¡El backend está funcionando! Usa /api/ para acceder a la API.", content_type="text/plain")),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('api/users/register/', createUserView.as_view(), name='create_user'),
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api-auth/", include("rest_framework.urls")),
-    path('api/', include('api.urls')),
 ]
